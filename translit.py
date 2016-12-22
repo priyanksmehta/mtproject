2fa20241c24b6bd70987d890b7e68e04f0e6f85f
priyanksmehta <priyanksanjaymehta@gmail.com>
2016-12-22 10:58:55 -0800

    resolved space issue

--- translit.py
+++ translit.py
@@ -1,31 +1,29 @@
 from maps import CONSONANTS, VOWELS
 import re
+
 class Transliterate:
 
 	def __init__(self):
 		self.schwa = 'a'
 
-	#necessary?
-	def _modify_schwa(self, inp, add = True):
-		if add:
-			inp += self.schwa
-		else:
-			inp = inp[:-1]
-
 	def romanize(self, hindi_text):
-		english_text = ''
-		for letter in hindi_text:
+		english_text = []
+		for letter in hindi_text.split(' '):
 			if letter in CONSONANTS.keys():
-				english_text += CONSONANTS[letter]
+				english_text.append(CONSONANTS[letter])
 				english_text += self.schwa
 			elif letter in VOWELS.keys():
-				english_text = english_text[:-1]
-				english_text += VOWELS[letter]
-			elif letter == ' ':
-				continue
+				english_text.pop()
+				english_text.append(VOWELS[letter])
 			else:
 				english_text = english_text[:-1]
 		if english_text[-1] == 'a':
 			return english_text[:-1]
 		else:
-			return english_text
+			return ''.join(english_text)
+
+#Usage
+if __name__ == '__main__':
+	test = Transliterate()
+	sent = input("Enter Hindi text:\n")
+	print(test.romanize(sent))
